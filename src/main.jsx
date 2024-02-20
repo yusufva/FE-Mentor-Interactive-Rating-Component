a4ce1d1fe179892b2aecefa8adcdcd4971b7586d
yusufva <yusufvalent@gmail.com>
2024-02-20 12:25:22 +0700

    fix: changing route

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RatingPage from "./Pages/rating";
+// import ThankyouPage from "./Pages/thankyou";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RatingPage />,
-  },
+    {
+        path: "/",
+        element: <RatingPage />,
+    },
+    // {
+    //   path: "/thankyou/:id",
+    //   element: <ThankyouPage />,
+    // },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
