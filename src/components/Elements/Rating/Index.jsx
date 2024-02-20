9c95ae543c2f3088d3137d6618207e59daf8300d
yusufva <yusufvalent@gmail.com>
2024-02-20 12:25:54 +0700

    refactor: refactoring code

--- src/components/Elements/Rating/Index.jsx
+++ src/components/Elements/Rating/Index.jsx
@@ -1,20 +1,20 @@
 const Rating = (props) => {
-  const {
-    id = 0,
-    handleChoice,
-    children,
-    classname = "bg-[#7c8798] hover:bg-[#959eac] ",
-    type = "button",
-  } = props;
-  return (
-    <button
-      className={`h-11 w-11 rounded-full text-[#959eac] ${classname}`}
-      onClick={() => handleChoice(id)}
-      type={type}
-    >
-      {children}
-    </button>
-  );
+    const {
+        id = 0,
+        handleChoice,
+        children,
+        classname = "bg-[#7c8798] hover:bg-[#959eac] ",
+        type = "button",
+    } = props;
+    return (
+        <button
+            className={`h-11 w-11 rounded-full text-[#959eac] ${classname}`}
+            onClick={() => handleChoice(id)}
+            type={type}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Rating;
