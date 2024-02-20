4d157ad4cec06ef4d039cf07215fe95ba7c40aab
yusufva <yusufvalent@gmail.com>
2024-02-20 12:28:25 +0700

    feature: adding conditional render for submit

--- src/Pages/rating.jsx
+++ src/Pages/rating.jsx
@@ -1,62 +1,71 @@
 import CardRating from "../components/Fragments/CardRating";
+import CardThankyou from "../components/Fragments/CardThankyou";
 import Rating from "../components/Elements/Rating/Index";
 import { useEffect, useState } from "react";
 
 const rating = [
-  {
-    id: 1,
-  },
-  {
-    id: 2,
-  },
-  {
-    id: 3,
-  },
-  {
-    id: 4,
-  },
-  {
-    id: 5,
-  },
+    {
+        id: 1,
+    },
+    {
+        id: 2,
+    },
+    {
+        id: 3,
+    },
+    {
+        id: 4,
+    },
+    {
+        id: 5,
+    },
 ];
 
 const RatingPage = () => {
-  const [isChoice, setIsChoice] = useState();
+    const [isChoice, setIsChoice] = useState();
+    const [isSubmited, setIsSubmited] = useState(false);
 
-  useEffect(() => {}, [isChoice]);
+    useEffect(() => {}, [isChoice]);
 
-  const handleChoice = (id) => {
-    setIsChoice(id);
-  };
-  return (
-    <div>
-      <div className="flex justify-center min-h-screen items-center bg-[#121417]">
-        <CardRating>
-          {console.log(isChoice)}
-          <CardRating.Header />
-          <CardRating.Body>
-            {rating.map((rating) => {
-              return (
-                <Rating
-                  key={rating.id}
-                  id={rating.id}
-                  classname={
-                    isChoice === rating.id
-                      ? "bg-orange-500 text-white"
-                      : "hover:bg-[#959eac] hover:text-white bg-gray-800"
-                  }
-                  handleChoice={handleChoice}
-                >
-                  {rating.id}
-                </Rating>
-              );
-            })}
-          </CardRating.Body>
-          <CardRating.Footer />
-        </CardRating>
-      </div>
-    </div>
-  );
+    const handleChoice = (id) => {
+        setIsChoice(id);
+    };
+    const handleSubmit = () => {
+        setIsSubmited(true);
+    };
+    return (
+        <div className="flex justify-center min-h-screen items-center bg-[#121417]">
+            {isSubmited ? (
+                <CardThankyou id={isChoice} />
+            ) : (
+                <CardRating>
+                    <CardRating.Header />
+                    <CardRating.Body>
+                        {rating.map((rating) => {
+                            return (
+                                <Rating
+                                    key={rating.id}
+                                    id={rating.id}
+                                    classname={
+                                        isChoice === rating.id
+                                            ? "bg-orange-500 text-white"
+                                            : "hover:bg-[#959eac] hover:text-white bg-gray-800"
+                                    }
+                                    handleChoice={handleChoice}
+                                >
+                                    {rating.id}
+                                </Rating>
+                            );
+                        })}
+                    </CardRating.Body>
+                    <CardRating.Footer
+                        id={isChoice}
+                        handleSubmit={handleSubmit}
+                    />
+                </CardRating>
+            )}
+        </div>
+    );
 };
 
 export default RatingPage;
